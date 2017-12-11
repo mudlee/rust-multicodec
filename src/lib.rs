df16c3de13bf0bff88c4ddb039cd7a681cfbae83
mudlee <contact@mudlee.hu>
2017-12-11 21:59:01 +0100

    starting the refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,109 +1,139 @@
 //! This library is a Rust implementation of the https://github.com/multiformats/multicodec project.
 extern crate integer_encoding;
 
-use std::io::Write;
-use integer_encoding::VarInt;
-
 mod codec_map;
 
-/// Returns the data prefixed with the codec's code in a u8 buffer.
-///
-/// # Arguments
-///
-/// * `codec_code` - The codec code, eg. 'base1'
-/// * `data` - the data to be prefixed
-///
-/// # Example
-///
-/// ```
-/// extern crate rust_multicodec;
-/// extern crate hex_slice;
-///
-/// use hex_slice::AsHex;
-/// use std::process;
-///
-/// fn main(){
-///     let data="Live long and prosper";
-///
-///     println!("{:X}",rust_multicodec::add_prefix("base1",data.as_bytes()).unwrap().as_hex());
-///     // it will print [1 4C 69 76 65 20 6C 6F 6E 67 20 61 6E 64 20 70 72 6F 73 70 65 72]
-/// }
-/// ```
-///
-pub fn add_prefix(codec_code: &str, data: &[u8]) -> Result<Vec<u8>, &'static str> {
-    match codec_map::get_decimal_by_code(codec_code) { // getting hex code of the codec
-        Some(decimal) => {
-            // encoding codec's (as decimal) into a varint
-            let mut target:Vec<u8>=decimal.encode_var_vec();
+mod codec_prefix {
+    use codec_map;
+    use std::io::Write;
+    use integer_encoding::VarInt;
 
-            match target.write(data) {
-                Err(_) => return Err("Could not write data into the result buffer"),
-                _=> ()
+    /// Returns the data prefixed with the codec's code in a u8 buffer.
+    ///
+    /// # Arguments
+    ///
+    /// * `codec_code` - The codec code, eg. 'base1'
+    /// * `data` - the data to be prefixed
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// extern crate rust_multicodec;
+    /// extern crate hex_slice;
+    ///
+    /// use hex_slice::AsHex;
+    /// use std::process;
+    ///
+    /// fn main(){
+    ///     let data="Live long and prosper";
+    ///
+    ///     println!("{:X}",rust_multicodec::add_prefix("base1",data.as_bytes()).unwrap().as_hex());
+    ///     // it will print [1 4C 69 76 65 20 6C 6F 6E 67 20 61 6E 64 20 70 72 6F 73 70 65 72]
+    /// }
+    /// ```
+    ///
+    pub fn add_prefix(codec_code: &str, data: &[u8]) -> Result<Vec<u8>, &'static str> {
+        match codec_map::get_decimal_by_code(codec_code) { // getting hex code of the codec
+            Some(decimal) => {
+                // encoding codec's (as decimal) into a varint
+                let mut target:Vec<u8>=decimal.encode_var_vec();
+
+                match target.write(data) {
+                    Err(_) => return Err("Could not write data into the result buffer"),
+                    _=> ()
+                }
+
+                Ok(target)
             }
-
-            Ok(target)
+            None => Err("No implementation for the given codec")
         }
-        None => Err("No implementation for the given codec")
+    }
+
+    /// Returns the codec's code the data was prefixed with.
+    ///
+    /// # Arguments
+    ///
+    /// * `data` - the data with prefix
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// extern crate rust_multicodec;
+    /// extern crate hex_slice;
+    ///
+    /// use hex_slice::AsHex;
+    /// use std::process;
+    ///
+    /// fn main(){
+    ///     let data="Live long and prosper";
+    ///
+    ///     let prefixed=rust_multicodec::add_prefix("base1",data.as_bytes()).unwrap();
+    ///     println!("{}",rust_multicodec::get_codec(prefixed.as_slice()).unwrap().unwrap());
+    ///     // it will print "base1"
+    /// }
+    /// ```
+    ///
+    pub fn get_codec(data: &[u8]) -> Result<Option<&'static str>, &'static str> {
+        let decoded:(u64,usize)=u64::decode_var_vec(&Vec::from(data));
+        Ok(codec_map::get_code_by_decimal(decoded.0))
+    }
+
+    /// Removes the codec prefix and returns the raw data.
+    ///
+    /// # Arguments
+    ///
+    /// * `data` - the data with prefix
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// extern crate rust_multicodec;
+    /// extern crate hex_slice;
+    ///
+    /// use hex_slice::AsHex;
+    /// use std::process;
+    ///
+    /// fn main(){
+    ///     let data="Live long and prosper";
+    ///
+    ///     let prefixed=rust_multicodec::add_prefix("base1",data.as_bytes()).unwrap();
+    ///     let raw_data=rust_multicodec::remove_prefix(prefixed.as_slice()).unwrap();
+    ///     println!("Original data was {:?}", String::from_utf8(raw_data).unwrap())
+    ///     // it will print return "Original data was Live long and prosper"
+    /// }
+    /// ```
+    ///
+    pub fn remove_prefix(data:&[u8]) -> Result<Vec<u8>, &'static str>{
+        let decoded:(u64,usize)=u64::decode_var_vec(&Vec::from(data));
+        Ok(data[decoded.1..].to_vec())
     }
 }
 
-/// Returns the codec's code the data was prefixed with.
-///
-/// # Arguments
-///
-/// * `data` - the data with prefix
-///
-/// # Example
-///
-/// ```
-/// extern crate rust_multicodec;
-/// extern crate hex_slice;
-///
-/// use hex_slice::AsHex;
-/// use std::process;
-///
-/// fn main(){
-///     let data="Live long and prosper";
-///
-///     let prefixed=rust_multicodec::add_prefix("base1",data.as_bytes()).unwrap();
-///     println!("{}",rust_multicodec::get_codec(prefixed.as_slice()).unwrap().unwrap());
-///     // it will print "base1"
-/// }
-/// ```
-///
-pub fn get_codec(data: &[u8]) -> Result<Option<&'static str>, &'static str> {
-    let decoded:(u64,usize)=u64::decode_var_vec(&Vec::from(data));
-    Ok(codec_map::get_code_by_decimal(decoded.0))
+
+
+pub mod x{
+    pub fn y(){}
 }
 
-/// Removes the codec prefix and returns the raw data.
-///
-/// # Arguments
-///
-/// * `data` - the data with prefix
-///
-/// # Example
-///
-/// ```
-/// extern crate rust_multicodec;
-/// extern crate hex_slice;
-///
-/// use hex_slice::AsHex;
-/// use std::process;
-///
-/// fn main(){
-///     let data="Live long and prosper";
-///
-///     let prefixed=rust_multicodec::add_prefix("base1",data.as_bytes()).unwrap();
-///     let raw_data=rust_multicodec::remove_prefix(prefixed.as_slice()).unwrap();
-///     println!("Original data was {:?}", String::from_utf8(raw_data).unwrap())
-///     // it will print return "Original data was Live long and prosper"
-/// }
-/// ```
-///
-pub fn remove_prefix(data:&[u8]) -> Result<Vec<u8>, &'static str>{
-    let decoded:(u64,usize)=u64::decode_var_vec(&Vec::from(data));
-    Ok(data[decoded.1..].to_vec())
+pub mod encoding {
+    use codec_prefix;
+
+    #[derive(Debug)]
+    pub enum Codec {
+        JSON
+    }
+
+    pub fn encode<T>(codec: Codec, object: &T) -> Result<Vec<u8>,&'static str>{
+        let data="Live long and prosper";
+        println!("{:?}",codec);
+        //add_prefix("json",data.as_bytes());
+        let prefix = match codec {
+            Codec::JSON => codec_prefix::add_prefix("json",data.as_bytes()),
+            _ => Err("No implementation for the given Codec")
+        };
+
+        prefix
+    }
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+//https://developers.google.com/protocol-buffers/docs/encoding
+//https://github.com/multiformats/multicodec
+//https://github.com/multiformats/unsigned-varint
+
+
 /*
 Van egy block data, mondjuk {"mySecret":"This is my secret"}. Ezt el akarjuk tárolni ilyesmi formában: db.store(key, value), ahol a key egy hash, a value egy encoded formája a secret-nek.
 Kell két module. 1: egy multiencode/decode modul, aminek beadod a block data-t, megmondod, hogy milyen encode-al kodolja el (json, xml, stb.).
@@ -10,20 +15,25 @@ a kimenetnek nem sztringnek, hanem binarisnak kell lennie, ami szoveges formatum
 Az <xml> prefix is egy int ertek, 1 vagy 2 bajton
 */
 
+extern crate rust_multicodec;
+
+use rust_multicodec::encoding;
+use rust_multicodec::encoding::Codec;
+
 mod multicodec {
-    pub struct DecodeResult {
+    /*pub struct DecodeResult {
         pub codec: Encoding,
         pub data: Vec<u8>
     }
 
-    impl DecodeResult{
+    /*impl DecodeResult{
         pub fn rawData(&self) -> &Object {
             // TODO returns the original object that was encoded
         }
-    }
+    }*/
 
     pub enum Encoding {
-        json=1
+        JSON
     }
 
     /// Encodes a the given object using the encode parameter
@@ -39,31 +49,31 @@ mod multicodec {
     /// And it will return
     /// # Returns
     /// It returns a u8 vector, where the first element is the codec itself, the rest is the byte verison of {"codec":"json","data": BINARY_VERSION_OF_THE_GIVEN_DATA }
-    pub fn encode(encode: Encoding, object: Object) -> Result<Vec<u8>,&'static str>{
+    pub fn encode<T>(encode: Encoding, object: &T) -> Result<Vec<u8>,&'static str>{
+        codec_prefix;
         // TODO
-
+        Ok(Vec::new())
     }
 
-    /// Decodes the given data
-    ///
-    /// # Arguments
-    ///
-    /// * `data` - the data that was encoded before
-    ///
-    /// # Returns
-    /// Returns a DecodeResult
-    pub fn decode(data: Vec<u8>) -> Result<DecodeResult,&'static str> {
+    /*pub fn decode(data: Vec<u8>) -> Result<DecodeResult,&'static str> {
 
-    }
+    }*/*/
+}
+
+struct Person {
+    name: String
 }
 
 fn main(){
+    let person=Person{name:String::from("sanyi")};
+    let result= encoding::encode(Codec::JSON, &person);
+    println!("{:?}",result);
     // sample usage:
-    let encoded:Vec<u8>=multicodec::encode(multicodec::Encoding::json, "any kind of data. bson, json, xml, anything");
+   /* let encoded:Vec<u8>=multicodec::encode(multicodec::Encoding::json, "any kind of data. bson, json, xml, anything");
     let decoded:multicodec::DecodeResult=multicodec::decode(encoded);
 
     decoded.codec; // json
     decoded.data; // binary version of the "any kind of data. bson, json, xml, anything"
-    decoded.rawData(); // "any kind of data. bson, json, xml, anything"
+    decoded.rawData(); // "any kind of data. bson, json, xml, anything"*/
 
 }
